3a7b1efea8c13499251d6fcead80a0f2a48d97c2
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-11-25 01:57:42 +0200

    Update gallery-api.ts

--- src/gallery-api.ts
+++ src/gallery-api.ts
@@ -1,8 +1,18 @@
 import axios from 'axios';
+import { Image } from './types';
 
 axios.defaults.baseURL = 'https://api.unsplash.com';
 
-export async function fetchGallery(searchValue, page) {
+type FetchGallery = {
+  results: Image[];
+  total_pages: number;
+  total: number;
+};
+
+export async function fetchGallery(
+  searchValue: string,
+  page: number
+): Promise<FetchGallery> {
   const response = await axios.get('/search/photos', {
     params: {
       client_id: 'uYXw3BB_tc3ywt_T3NF5yYA8n7c37PAMn6uMTzXMS0U',
